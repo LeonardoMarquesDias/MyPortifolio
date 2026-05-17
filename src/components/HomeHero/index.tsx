c2e58afbd0b53025514ed9bda787ac3e8a5128a6
Leonardo Dias <leonardolaureanomarquesdias@gmail.com>
2026-05-17 03:12:35 +0100

    Update HomeHero with real profile data

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -4,40 +4,40 @@ import { Sidebar } from '../Sidebar';
 function HomeHero() {
   return (
     <Container data-aos="fade-up">
-      
+
       <Sidebar />
       <div>
         <TextContainer>
-          <h1>Hello...</h1>
-          <h2>Welcome</h2>
+          <h1>Leonardo Dias</h1>
+          <h2>SEO Executive & Front-End Developer</h2>
         </TextContainer>
         <InfosContainer>
           <CodeItem data-aos="zoom-in">
-            <span className="purple">Infos</span> {'\u007B'}
+            <span className="purple">Profile</span> {'\u007B'}
             <div>
-              Name: <span className="blue">Leonardo,</span>
+              Name: <span className="blue">Leonardo Laureano,</span>
             </div>
             <div>
-              Surname: <span className="blue">Dias,</span>
+              Location: <span className="blue">Swindon, UK,</span>
             </div>
             <div>
-              Location: <span className="blue">United Kingdom</span>
+              Focus: <span className="blue">SEO / GEO / Front-End</span>
             </div>
             {'\u007D'}
           </CodeItem>
           <CodeItem data-aos="zoom-in">
-            <span className="purple">Job</span> {'\u007B'}
+            <span className="purple">Expertise</span> {'\u007B'}
             <div>
-              Developer: <span className="blue">Front-End,</span>
+              SEO: <span className="blue">On-Page, Off-Page, Technical,</span>
             </div>
             <div>
-              Technology: <span className="blue">ReactJS,</span>
+              Tools: <span className="blue">SEMrush, GA4, GSC,</span>
             </div>
             <div>
-              Learning: <span className="blue">NextJS,</span>
+              Dev: <span className="blue">React, Next.js, WordPress,</span>
             </div>
             <div>
-              Design: <span className="blue">UI</span>
+              Platforms: <span className="blue">Google, Bing, AI Search</span>
             </div>
             {'\u007D'}
           </CodeItem>
