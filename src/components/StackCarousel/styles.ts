a6dc0e8e4cd377969356f7f760c5b4ec7a6810f7
Leonardo Dias <leonardolaureanomarquesdias@gmail.com>
2026-05-19 02:55:04 +0100

    Updated styles.ts

--- src/components/StackCarousel/styles.ts
+++ src/components/StackCarousel/styles.ts
@@ -8,6 +8,7 @@ const scroll = keyframes`
 export const Wrapper = styled.div`
   width: 100%;
   overflow: hidden;
+  margin-top: 6rem;
   border-top: 1px solid ${({ theme }) => theme.border};
   border-bottom: 1px solid ${({ theme }) => theme.border};
   padding: 1.5rem 0;
