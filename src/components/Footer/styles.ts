cec227d76edfd22bcaeea236567aa7172ca4cd73
Leonardo Dias <leonardolaureanomarquesdias@gmail.com>
2026-05-19 02:57:22 +0100

    Updated styles.ts

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.footer`
   width: 100%;
   display: flex;
-  margin-top: 10rem;
+  margin-top: 3rem;
   border-top: 1px solid ${({ theme }) => theme.border};
   min-height: 5rem;
   align-items: center;
